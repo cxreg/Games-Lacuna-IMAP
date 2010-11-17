ca3f7429d93775bcfa94327fef27698e02f2e1fc
Dave Olszewski <dolszewski@marchex.com>
2010-11-17 01:18:03 -0800

    use Client directly from Auth, no need for a RPC class

--- lib/Games/Lacuna/IMAP/Auth.pm
+++ lib/Games/Lacuna/IMAP/Auth.pm
@@ -1,17 +1,21 @@
 package Games::Lacuna::IMAP::Auth;
 
 use base 'Net::IMAP::Server::DefaultAuth';
-use aliased Games::Lacuna::IMAP::RPC;
+
+use aliased Games::Lacuna::IMAP::Conf;
+use Games::Lacuna::Client;
 
 sub auth_plain {
     my ($self, $user, $pass) = @_;
 
-    my $lacuna = RPC->new(
+    my $self->{lacuna} = Games::Lacuna::Client->new(
+        uri      => Conf->uri,
+        api_key  => Conf->api_key,
         name     => $user,
         password => $pass,
     );
 
-    if ($lacuna->assert_session) {
+    if ($self->{lacuna}->assert_session) {
         return 1;
     } else {
         return;
