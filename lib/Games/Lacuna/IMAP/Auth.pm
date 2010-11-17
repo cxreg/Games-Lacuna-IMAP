8b9048a91bfdb3ca8ee04aea692577e820b2fae5
Dave Olszewski <dolszewski@marchex.com>
2010-11-17 01:56:17 -0800

    make accessor for lacuna client, and set user upon login

--- lib/Games/Lacuna/IMAP/Auth.pm
+++ lib/Games/Lacuna/IMAP/Auth.pm
@@ -5,17 +5,20 @@ use base 'Net::IMAP::Server::DefaultAuth';
 use aliased Games::Lacuna::IMAP::Conf;
 use Games::Lacuna::Client;
 
+__PACKAGE__->mk_accessors(qw(lacuna));
+
 sub auth_plain {
     my ($self, $user, $pass) = @_;
 
-    my $self->{lacuna} = Games::Lacuna::Client->new(
+    $self->lacuna(Games::Lacuna::Client->new(
         uri      => Conf->uri,
         api_key  => Conf->api_key,
         name     => $user,
         password => $pass,
-    );
+    ));
 
-    if ($self->{lacuna}->assert_session) {
+    if ($self->lacuna->assert_session) {
+        $self->user($user);
         return 1;
     } else {
         return;
