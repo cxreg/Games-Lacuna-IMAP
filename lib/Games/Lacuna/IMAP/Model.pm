b939a37a5807cab5b0c522d80b4e4f756828ecb1
Dave Olszewski <dolszewski@marchex.com>
2010-11-17 01:56:51 -0800

    bare bones model

--- lib/Games/Lacuna/IMAP/Model.pm
+++ lib/Games/Lacuna/IMAP/Model.pm
@@ -1,5 +1,13 @@
 package Games::Lacuna::IMAP::Model;
 
 use base 'Net::IMAP::Server::DefaultModel';
+use Games::Lacuna::IMAP::Mailbox;
+
+sub init {
+    my ($self) = @_;
+
+    $self->root(Games::Lacuna::IMAP::Mailbox->new);
+    $self->root->add_child(name => 'INBOX');
+}
 
 1;
